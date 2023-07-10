0f6716924d9577ce5820740369dd87ad6391c173
Luis Zubia <soyzubia@gmail.com>
2023-07-10 09:36:13 -0700

    Add query to answer: List the name of all animals born between 2016 and 2019

--- queries.sql
+++ queries.sql
@@ -2,3 +2,6 @@
 
 /* - Find all animals whose name ends in "mon". */
 SELECT * FROM animals WHERE name LIKE '%mon';
+
+/* - List the name of all animals born between 2016 and 2019. */
+SELECT name FROM animals WHERE date_of_birth BETWEEN '01/01/2016' AND '12/31/2019';
