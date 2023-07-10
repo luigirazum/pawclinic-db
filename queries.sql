937e5eef7aedce4597865abaa9bb423efdbca130
Luis Zubia <soyzubia@gmail.com>
2023-07-10 10:43:38 -0700

    Apply required change: Leave just one query for answer the query 'Find all animals with a weight between 10.4Kg and 17.3Kg

--- queries.sql
+++ queries.sql
@@ -24,4 +24,3 @@ SELECT * FROM animals WHERE name NOT IN ('Gabumon');
 /* - Find all animals with a weight between 10.4kg and 17.3kg */
 /*   (including the animals with the weights that equals precisely 10.4kg or 17.3kg) */
 SELECT * FROM animals WHERE weight_kg BETWEEN 10.4 AND 17.3;
-SELECT * FROM animals WHERE weight_kg >= 10.4 AND weight_kg <= 17.3;
