d64f868781f3a4c80e0eb3858ed4edce3803ca51
Luis Zubia <soyzubia@gmail.com>
2023-07-10 09:47:11 -0700

    Add query to answer: Find all animals not named Gabumon

--- queries.sql
+++ queries.sql
@@ -17,3 +17,6 @@ SELECT name, escape_attempts FROM animals WHERE weight_kg > 10.5;
 
 /* - Find all animals that are neutered. */
 SELECT * FROM animals WHERE neutered=true;
+
+/* - Find all animals not named Gabumon. */
+SELECT * FROM animals WHERE name NOT IN ('Gabumon');
