27e2e12567791b5f6a8dfd16ba2be14416b1892c
Luis Zubia <soyzubia@gmail.com>
2023-07-10 10:58:21 -0700

    Apply required change: Correct date_of_birth using YYYY/MM/DD format for Pikachu animal

--- data.sql
+++ data.sql
@@ -7,7 +7,7 @@ INSERT INTO animals(name, date_of_birth, escape_attempts, neutered, weight_kg)
   VALUES ('Gabumon', '2018/11/15', 2, true, 8);
 
 INSERT INTO animals(name, date_of_birth, escape_attempts, neutered, weight_kg)
-  VALUES ('Pikachu', '06/07/2021', 1, false, 15.04);
+  VALUES ('Pikachu', '2021/01/07', 1, false, 15.04);
 
 INSERT INTO animals(name, date_of_birth, escape_attempts, neutered, weight_kg)
   VALUES ('Devimon', '05/12/2017', 5, true, 11);
