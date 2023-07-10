15cb135c1917562dcdefae8d3c6e624a481a815d
Luis Zubia <soyzubia@gmail.com>
2023-07-10 12:55:53 -0700

    Modify 'animals' table: Add a column 'species' of type 'string'

--- schema.sql
+++ schema.sql
@@ -11,3 +11,8 @@ CREATE TABLE animals (
     weight_kg DECIMAL,
     PRIMARY KEY(id)
 );
+
+/* Start query-update-animals milestone */
+/* --------------------------------------- */
+/* Add a column `species` of type `string` to your `animals` table */
+ALTER TABLE animals ADD species VARCHAR(50);
