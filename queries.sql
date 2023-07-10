3e23739a6c19bf21a34d3b2e3bc71fa6eade6dde
Luis Zubia <soyzubia@gmail.com>
2023-07-10 13:29:57 -0700

    Inside a transaction:   - Update the `animals` table by       setting the `species` column to `digimon` for all animals that have a name ending in `mon`.   - Update the `animals` table by:       setting the `species` column to `pokemon` for all animals that don't have `species` already set.   - Verify that changes were made.   - Commit the transaction.   - Verify that changes persist after commit.

--- queries.sql
+++ queries.sql
@@ -38,3 +38,19 @@ UPDATE animals SET species='unspecified';
 SELECT * FROM animals;
 ROLLBACK;
 SELECT * FROM animals;
+
+/* Inside a transaction: */
+/*  - Update the `animals` table by */
+/*      setting the `species` column to `digimon` for all animals that have a name ending in `mon`. */
+/*  - Update the `animals` table by: */
+/*      setting the `species` column to `pokemon` for all animals that don't have `species` already set. */
+/*  - Verify that changes were made. */
+/*  - Commit the transaction. */
+/*  - Verify that changes persist after commit. */
+BEGIN;
+UPDATE animals SET species='digimon' WHERE name LIKE '%mon';
+SELECT * FROM animals;
+UPDATE animals SET species='pokemon' WHERE species ISNULL;
+SELECT * FROM animals;
+COMMIT;
+SELECT * FROM animals;
