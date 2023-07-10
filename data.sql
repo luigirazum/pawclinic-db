bbdc7699dc39b0ddd8d0cc81e5c0e6fbcccd3bd1
Luis Zubia <soyzubia@gmail.com>
2023-07-10 10:54:03 -0700

    Apply required change: Correct date_of_birth using YYYY/MM/DD format for Agumon animal

--- data.sql
+++ data.sql
@@ -1,7 +1,7 @@
 /* Populate database with sample data. */
 
 INSERT INTO animals(name, date_of_birth, escape_attempts, neutered, weight_kg)
-  VALUES ('Agumon', '02/03/2020', 0, true, 10.23);
+  VALUES ('Agumon', '2020/02/03', 0, true, 10.23);
 
 INSERT INTO animals(name, date_of_birth, escape_attempts, neutered, weight_kg)
   VALUES ('Gabumon', '11/15/2018', 2, true, 8);
@@ -11,4 +11,3 @@ INSERT INTO animals(name, date_of_birth, escape_attempts, neutered, weight_kg)
 
 INSERT INTO animals(name, date_of_birth, escape_attempts, neutered, weight_kg)
   VALUES ('Devimon', '05/12/2017', 5, true, 11);
-  
