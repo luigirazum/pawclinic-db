5a04f85239c562c20905cd5e4286956deed65227
Luis Zubia <soyzubia@gmail.com>
2023-07-12 04:56:15 -0700

    Modify your inserted animals so it includes the `species_id` value:   - All other animals are Pokemon

--- data.sql
+++ data.sql
@@ -59,3 +59,9 @@ UPDATE animals
     WHERE name='Digimon')
   WHERE name LIKE '%mon';
 
+/*  - All other animals are Pokemon */
+UPDATE animals
+  SET species_id=(SELECT id
+    FROM species
+    WHERE name='Pokemon')
+  WHERE name NOT LIKE '%mon';
