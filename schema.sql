8c5dc6fb0b9ff8b14dace3623a1eb59433b8f898
Luis Zubia <soyzubia@gmail.com>
2023-07-12 04:08:15 -0700

    Create a table named 'owners' with the following columns:   - id: integer (set it as autoincremented PRIMARY KEY),   - full_name: string,   - age: integer

--- schema.sql
+++ schema.sql
@@ -16,3 +16,15 @@ CREATE TABLE animals (
 /* --------------------------------------- */
 /* Add a column `species` of type `string` to your `animals` table */
 ALTER TABLE animals ADD species VARCHAR(50);
+
+/* Start queries-with-multiple-tables milestone */
+/* --------------------------------------- */
+/* Create a table named 'owners' with the following columns: */
+/*  - id: integer (set it as autoincremented PRIMARY KEY), */
+/*  - full_name: string, */
+/*  - age: integer */
+CREATE TABLE owners(
+    id SERIAL PRIMARY KEY,
+    full_name VARCHAR(255),
+    age INT
+);
