815768870f19253c9d7a340348bfc1fea126cfe1
Luis Zubia <soyzubia@gmail.com>
2023-07-13 04:18:41 -0700

    Insert the following data for visits: - Agumon visited William Tatcher on May 24th, 2020. - Agumon visited Stephanie Mendez on Jul 22th, 2020. - Gabumon visited Jack Harkness on Feb 2nd, 2021. - Pikachu visited Maisy Smith on Jan 5th, 2020. - Pikachu visited Maisy Smith on Mar 8th, 2020. - Pikachu visited Maisy Smith on May 14th, 2020. - Devimon visited Stephanie Mendez on May 4th, 2021. - Charmander visited Jack Harkness on Feb 24th, 2021. - Plantmon visited Maisy Smith on Dec 21st, 2019. - Plantmon visited William Tatcher on Aug 10th, 2020. - Plantmon visited Maisy Smith on Apr 7th, 2021. - Squirtle visited Stephanie Mendez on Sep 29th, 2019. - Angemon visited Jack Harkness on Oct 3rd, 2020. - Angemon visited Jack Harkness on Nov 4th, 2020. - Boarmon visited Maisy Smith on Jan 24th, 2019. - Boarmon visited Maisy Smith on May 15th, 2019. - Boarmon visited Maisy Smith on Feb 27th, 2020. - Boarmon visited Maisy Smith on Aug 3rd, 2020. - Blossom visited Stephanie Mendez on May 24th, 2020. - Blossom visited William Tatcher on Jan 11th, 2021.

--- data.sql
+++ data.sql
@@ -138,3 +138,129 @@ INSERT INTO specializations
       (SELECT id FROM vets WHERE name='Jack Harkness'),
       (SELECT id FROM species WHERE name='Digimon')
     );
+
+
+/* Insert the following data for visits: */
+/*  - Agumon visited William Tatcher on May 24th, 2020. */
+/*  - Agumon visited Stephanie Mendez on Jul 22th, 2020. */
+/*  - Gabumon visited Jack Harkness on Feb 2nd, 2021. */
+/*  - Pikachu visited Maisy Smith on Jan 5th, 2020. */
+/*  - Pikachu visited Maisy Smith on Mar 8th, 2020. */
+/*  - Pikachu visited Maisy Smith on May 14th, 2020. */
+/*  - Devimon visited Stephanie Mendez on May 4th, 2021. */
+/*  - Charmander visited Jack Harkness on Feb 24th, 2021. */
+/*  - Plantmon visited Maisy Smith on Dec 21st, 2019. */
+/*  - Plantmon visited William Tatcher on Aug 10th, 2020. */
+/*  - Plantmon visited Maisy Smith on Apr 7th, 2021. */
+/*  - Squirtle visited Stephanie Mendez on Sep 29th, 2019. */
+/*  - Angemon visited Jack Harkness on Oct 3rd, 2020. */
+/*  - Angemon visited Jack Harkness on Nov 4th, 2020. */
+/*  - Boarmon visited Maisy Smith on Jan 24th, 2019. */
+/*  - Boarmon visited Maisy Smith on May 15th, 2019. */
+/*  - Boarmon visited Maisy Smith on Feb 27th, 2020. */
+/*  - Boarmon visited Maisy Smith on Aug 3rd, 2020. */
+/*  - Blossom visited Stephanie Mendez on May 24th, 2020. */
+/*  - Blossom visited William Tatcher on Jan 11th, 2021. */
+
+INSERT INTO visits
+  VALUES
+    (
+      (SELECT id FROM animals WHERE name='Agumon'),
+      (SELECT id FROM vets WHERE name='William Tatcher'),
+      '2020/05/24'
+    ),
+    (
+      (SELECT id FROM animals WHERE name='Agumon'),
+      (SELECT id FROM vets WHERE name='Stephanie Mendez'),
+      '2020/07/22'
+    ),
+    (
+      (SELECT id FROM animals WHERE name='Gabumon'),
+      (SELECT id FROM vets WHERE name='Jack Harkness'),
+      '2021/02/02'
+    ),
+    (
+      (SELECT id FROM animals WHERE name='Pikachu'),
+      (SELECT id FROM vets WHERE name='Maisy Smith'),
+      '2020/01/05'
+    ),
+    (
+      (SELECT id FROM animals WHERE name='Pikachu'),
+      (SELECT id FROM vets WHERE name='Maisy Smith'),
+      '2020/03/08'
+    ),
+    (
+      (SELECT id FROM animals WHERE name='Pikachu'),
+      (SELECT id FROM vets WHERE name='Maisy Smith'),
+      '2020/05/14'
+    ),
+    (
+      (SELECT id FROM animals WHERE name='Devimon'),
+      (SELECT id FROM vets WHERE name='Stephanie Mendez'),
+      '2021/05/04'
+    ),
+    (
+      (SELECT id FROM animals WHERE name='Charmander'),
+      (SELECT id FROM vets WHERE name='Jack Harkness'),
+      '2021/02/24'
+    ),
+    (
+      (SELECT id FROM animals WHERE name='Plantmon'),
+      (SELECT id FROM vets WHERE name='Maisy Smith'),
+      '2019/12/21'
+    ),
+    (
+      (SELECT id FROM animals WHERE name='Plantmon'),
+      (SELECT id FROM vets WHERE name='William Tatcher'),
+      '2020/08/10'
+    ),
+    (
+      (SELECT id FROM animals WHERE name='Plantmon'),
+      (SELECT id FROM vets WHERE name='Maisy Smith'),
+      '2021/04/07'
+    ),
+    (
+      (SELECT id FROM animals WHERE name='Squirtle'),
+      (SELECT id FROM vets WHERE name='Stephanie Mendez'),
+      '2019/09/29'
+    ),
+    (
+      (SELECT id FROM animals WHERE name='Angemon'),
+      (SELECT id FROM vets WHERE name='Jack Harkness'),
+      '2020/10/03'
+    ),
+    (
+      (SELECT id FROM animals WHERE name='Angemon'),
+      (SELECT id FROM vets WHERE name='Jack Harkness'),
+      '2020/11/04'
+    ),
+    (
+      (SELECT id FROM animals WHERE name='Boarmon'),
+      (SELECT id FROM vets WHERE name='Maisy Smith'),
+      '2019/01/24'
+    ),
+    (
+      (SELECT id FROM animals WHERE name='Boarmon'),
+      (SELECT id FROM vets WHERE name='Maisy Smith'),
+      '2019/05/15'
+    ),
+    (
+      (SELECT id FROM animals WHERE name='Boarmon'),
+      (SELECT id FROM vets WHERE name='Maisy Smith'),
+      '2020/02/27'
+    ),
+    (
+      (SELECT id FROM animals WHERE name='Boarmon'),
+      (SELECT id FROM vets WHERE name='Maisy Smith'),
+      '2020/08/03'
+    ),
+    (
+      (SELECT id FROM animals WHERE name='Blossom'),
+      (SELECT id FROM vets WHERE name='Stephanie Mendez'),
+      '2020/05/24'
+    ),
+    (
+      (SELECT id FROM animals WHERE name='Blossom'),
+      (SELECT id FROM vets WHERE name='William Tatcher'),
+      '2021/01/11'
+    );
